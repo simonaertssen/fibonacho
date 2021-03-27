3beb81986795b7fa70e17232aa8e19c3c8bf36fa
Simon Aertssen <Simonaertssen@gmail.com>
2021-03-27 16:38:32 +0100

    Working on the app state. Dissociate between the action of blacklisting and the object of the blacklist

--- src/server_help.py
+++ src/server_help.py
@@ -1,3 +1,6 @@
+import json
+
+
 def to_positive_integer(n):
     """
     Make the input an integer and raise an error is there is a negative number
@@ -19,3 +22,20 @@ def validate_input(validate_me):
     except (TypeError, ValueError):
         raise
     return validate_me
+
+
+def save_app_state(the_blacklist: list):
+    """
+    Save the blacklist to disk as it should persist in application state
+    """
+    with open('../the_blacklist.json', 'w') as f:
+        json.dump(the_blacklist, f)
+
+
+def load_app_state():
+    """
+    Load the persisted blacklist
+    """
+    with open('../the_blacklist.json', 'r') as f:
+        the_blacklist = json.load(f)
+    return the_blacklist
